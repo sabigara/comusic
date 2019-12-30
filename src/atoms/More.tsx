d50fea372370099b88437a8bd702efbbaead7587
Isami Yuma <lemonburst1958@gmail.com>
2019-12-30 11:52:51 +0900

    Implement More component

---
+++ src/atoms/More.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const More: React.FC = () => {
+  return (
+    <Wrapper>
+      <Dot/>
+      <Dot/>
+      <Dot/>
+    </Wrapper>
+  )
+};
+
+const Wrapper = styled.div`
+  height: 17px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`
+
+const Dot = styled.div`
+  background-color: white;
+  border-radius: 50%;
+  width: 3px;
+  height: 3px;
+`
+
+export default More;
