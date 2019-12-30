91d410ae7b53ee9ffc996971415b023308e9f215
Isami Yuma <lemonburst1958@gmail.com>
2019-12-30 11:53:05 +0900

    Implement Label component

---
+++ src/atoms/Label.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Label = styled.span`
+  color: white;
+  font-family: sans-serif;
+  font-size: 14px;
+  cursor: default;
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Opera and Firefox */
+`
+
+export default Label;
