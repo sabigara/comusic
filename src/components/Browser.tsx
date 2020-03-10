dca32a79366a28ad2af7594946dc1fa02d814d4e
Isami Yuma <lemonburst1958@gmail.com>
2020-03-10 16:21:34 +0900

    Replace react-ui-tree with blueprint component

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { ReactElement, useCallback, useMemo } from 'react';
 import { useSelector, shallowEqual } from 'react-redux';
 import { Scrollbar } from 'react-scrollbars-custom';
-import NodeUI from 'react-ui-tree/dist/node';
-import Tree from 'react-ui-tree/dist/tree';
 import 'react-ui-tree/dist/react-ui-tree.css';
 import {
+  Classes,
+  Intent,
+  H5,
   Button,
   Dialog,
   Menu,
@@ -13,6 +14,8 @@ import {
   Popover,
   Position,
   Icon,
+  Tree,
+  ITreeNode,
 } from '@blueprintjs/core';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import styled from 'styled-components';
@@ -31,37 +34,63 @@ import { useAddSong, useDelSong } from '../hooks/songs';
 import { useAddVersion, useDelVersion } from '../hooks/versions';
 
 enum NodeKind {
-  Root,
   Studio,
   Song,
   Version,
 }
 
-type NodeData = StudioState | SongState | VersionState | { id: string };
+type NodeData = StudioState | SongState | VersionState;
 
 type Node = {
-  module: string;
   kind: NodeKind;
-  collapsed: boolean;
-  children: Node[];
   data: NodeData;
-};
-
-function isFolder(node: Node) {
-  return node.kind !== NodeKind.Version;
-}
+} & ITreeNode;
 
 // Recursively compare every nodes.
-function nodeDeepEquals(prev: Node, curr: Node): boolean {
-  if (prev.children.length != curr.children.length) {
+function nodeDeepEquals(prev?: Node, curr?: Node): boolean {
+  if (prev === undefined || curr === undefined) {
+    return prev === curr;
+  }
+  if (prev.childNodes === undefined || curr.childNodes === undefined) {
+    return prev.childNodes === curr.childNodes;
+  }
+  if (prev.childNodes?.length != curr.childNodes?.length) {
     return false;
   }
   return (
+    prev.id === curr.id &&
     shallowEqual(prev.data, curr.data) &&
-    prev.children.reduce((acc: boolean, child, i) => {
-      return acc && nodeDeepEquals(child, curr.children[i]);
+    prev.childNodes?.reduce((acc: boolean, child, i) => {
+      return (
+        acc &&
+        nodeDeepEquals(
+          child as Node,
+          curr.childNodes ? (curr.childNodes[i] as Node) : undefined,
+        )
+      );
     }, true)
   );
+}
+
+function nodeListDeepEquals(prev: Node[], curr: Node[]): boolean {
+  return prev.reduce((acc: boolean, child, i) => {
+    return acc && nodeDeepEquals(child as Node, curr[i] as Node);
+  }, true);
+}
+
+function iconForNodeKind(
+  nodeKind: NodeKind,
+): 'home' | 'music' | 'document' | 'error' {
+  switch (nodeKind) {
+    case NodeKind.Studio:
+      return 'home';
+    case NodeKind.Song:
+      return 'music';
+    case NodeKind.Version:
+      return 'document';
+    default:
+      return 'error';
+  }
 }
 
 type Props = {
@@ -72,56 +101,6 @@ const Browser: React.FC<Props> = ({ setVerId }) => {
   const user = useCurrentUser();
   // API call
   useFetchStudios(user?.id);
-
-  const [collapsedNodes, setCollapsedNodes] = React.useState<string[]>([]);
-  // Construct tree-like data structure for browser.
-  // Root -> Studio -> Song -> Version
-  const treeData = useSelector((state: RootState) => {
-    const studios: Node[] = state.studios.allIds.map((id) => {
-      const studio = state.studios.byId[id];
-      const songs: Node[] = state.songs.allIds
-        .map((id) => {
-          const song = state.songs.byId[id];
-          const versions: Node[] = state.versions.allIds
-            .map((id) => {
-              const ver = state.versions.byId[id];
-              return {
-                module: ver.name,
-                kind: NodeKind.Version,
-                children: [],
-                // Doc says "selector function should be pure".
-                // Maybe better not to rely on local state?
-                // https://react-redux.js.org/api/hooks#useselector
-                collapsed: collapsedNodes.includes(ver.id),
-                data: ver,
-              };
-            })
-            .filter((ver) => ver.data.songId === song.id);
-          return {
-            module: song.name,
-            kind: NodeKind.Song,
-            collapsed: collapsedNodes.includes(song.id),
-            children: versions,
-            data: song,
-          };
-        })
-        .filter((song) => song.data.studioId === studio.id);
-      return {
-        module: studio.name,
-        kind: NodeKind.Studio,
-        collapsed: collapsedNodes.includes(studio.id),
-        children: songs,
-        data: studio,
-      };
-    });
-    return {
-      module: 'root',
-      kind: NodeKind.Root,
-      collapsed: false,
-      children: studios,
-      data: { id: 'root' },
-    };
-  }, nodeDeepEquals);
 
   const backendAPI = useBackendAPI();
   const dispatchWithAPICall = useDispatchWithAPICall();
@@ -151,181 +130,235 @@ const Browser: React.FC<Props> = ({ setVerId }) => {
     setDialogOpen(false);
   };
 
-  const inviteDialog = (
-    <Dialog
-      icon="new-person"
-      onClose={() => {
-        setGroupId(null);
-        setDialogOpen(false);
-      }}
-      title="Invite Member to Studio"
-      isOpen={isDialogOpen}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          margin: '20px 20px 0 20px',
+  const inviteDialog = useMemo(
+    () => (
+      <Dialog
+        icon="new-person"
+        onClose={() => {
+          setGroupId(null);
+          setDialogOpen(false);
         }}
+        title="Invite Member to Studio"
+        isOpen={isDialogOpen}
+        usePortal={false}
       >
-        <InputGroup
-          large
-          leftIcon="envelope"
-          placeholder="test@example.com"
-          fill
-          style={{ marginBottom: 20 }}
-          onChange={(e: React.FormEvent<HTMLInputElement>) =>
-            setEmail(e.currentTarget.value)
-          }
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: '20px 20px 0 20px',
+          }}
+        >
+          <InputGroup
+            large
+            leftIcon="envelope"
+            placeholder="test@example.com"
+            fill
+            style={{ marginBottom: 20 }}
+            onChange={(e: React.FormEvent<HTMLInputElement>) =>
+              setEmail(e.currentTarget.value)
+            }
+          />
+          <Button text="Send Invitation" large onClick={onInviteSubmit} />
+        </div>
+      </Dialog>
+    ),
+    [setGroupId, setDialogOpen, setEmail, onInviteSubmit],
+  );
+
+  const genStudioMenu = useCallback(
+    (data: StudioState) => (
+      <Menu>
+        <MenuItem
+          icon="add"
+          text="Add Song"
+          onClick={() => {
+            addSong(data.id, 'new song');
+          }}
         />
-        <Button text="Send Invitation" large onClick={onInviteSubmit} />
+        <MenuItem
+          icon="new-person"
+          text="Invite"
+          onClick={() => {
+            setGroupId({ id: data.id, type: 'studio' });
+            setDialogOpen(true);
+          }}
+        />
+        {/* <MenuItem icon="edit" text="Rename" /> */}
+      </Menu>
+    ),
+    [addSong, setGroupId, setDialogOpen],
+  );
+
+  const genSongMenu = useCallback(
+    (data: SongState) => (
+      <Menu>
+        <MenuItem
+          icon="add"
+          text="Add Version"
+          onClick={() => {
+            addVersion(data.id, 'new version');
+          }}
+        />
+        <MenuItem
+          icon="new-person"
+          text="Invite"
+          onClick={() => {
+            setGroupId({ id: data.id, type: 'song' });
+            setDialogOpen(true);
+          }}
+        />
+        {/* <MenuItem icon="edit" text="Rename" /> */}
+        <MenuItem
+          icon="delete"
+          text="Delete"
+          onClick={() => {
+            delSong(data.id);
+          }}
+        />
+      </Menu>
+    ),
+    [addVersion, setGroupId, setDialogOpen, delSong],
+  );
+
+  const genVerMenu = useCallback(
+    (data: VersionState) => (
+      <Menu>
+        {/* <MenuItem icon="edit" text="Rename" /> */}
+        <MenuItem
+          icon="delete"
+          text="Delete"
+          onClick={() => {
+            delVersion(data.id);
+          }}
+        />
+      </Menu>
+    ),
+    [delVersion],
+  );
+
+  const confirmDelDialog = (
+    <div>
+      <H5>Confirm deletion</H5>
+      <p>
+        Are you sure you want to delete these items? You won't be able to
+        recover them.
+      </p>
+      <div
+        style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 15 }}
+      >
+        <Button className={Classes.POPOVER_DISMISS} style={{ marginRight: 10 }}>
+          Cancel
+        </Button>
+        <Button intent={Intent.DANGER} className={Classes.POPOVER_DISMISS}>
+          Delete
+        </Button>
       </div>
-    </Dialog>
+    </div>
   );
 
-  const genStudioMenu = (data: StudioState) => (
-    <Menu>
-      <MenuItem
-        icon="add"
-        text="Add Song"
-        onClick={() => {
-          addSong(data.id, 'new song');
-        }}
-      />
-      <MenuItem
-        icon="new-person"
-        text="Invite"
-        onClick={() => {
-          setGroupId({ id: data.id, type: 'studio' });
-          setDialogOpen(true);
-        }}
-      />
-      {/* <MenuItem icon="edit" text="Rename" /> */}
-    </Menu>
-  );
+  const [collapsedNodes, setCollapsedNodes] = React.useState<string[]>([]);
 
-  const genSongMenu = (data: SongState) => (
-    <Menu>
-      <MenuItem
-        icon="add"
-        text="Add Version"
-        onClick={() => {
-          addVersion(data.id, 'new version');
-        }}
-      />
-      <MenuItem
-        icon="new-person"
-        text="Invite"
-        onClick={() => {
-          setGroupId({ id: data.id, type: 'song' });
-          setDialogOpen(true);
-        }}
-      />
-      {/* <MenuItem icon="edit" text="Rename" /> */}
-      <MenuItem
-        icon="delete"
-        text="Delete"
-        onClick={() => {
-          delSong(data.id);
-        }}
-      />
-    </Menu>
-  );
-
-  const genVerMenu = (data: VersionState) => (
-    <Menu>
-      {/* <MenuItem icon="edit" text="Rename" /> */}
-      <MenuItem
-        icon="delete"
-        text="Delete"
-        onClick={() => {
-          delVersion(data.id);
-        }}
-      />
-    </Menu>
-  );
-
-  const handleNodeClick = (e: any, node: Node) => {
-    if (e.target.id !== 'folder-node') return;
-    if (isFolder(node)) {
-      const collapsed = collapsedNodes.includes(node.data.id);
-      node.collapsed = !collapsed;
-      collapsed
-        ? setCollapsedNodes(collapsedNodes.filter((id) => id !== node.data.id))
-        : setCollapsedNodes(collapsedNodes.concat(node.data.id));
-    } else {
-      setVerId(node.data.id);
-    }
-  };
-
-  const iconForNodeKind = (
-    nodeKind: NodeKind,
-  ): 'home' | 'music' | 'document' | 'error' => {
-    switch (nodeKind) {
+  const genNode = <T extends NodeData>(
+    data: T,
+    kind: NodeKind,
+    childNodes?: Node[],
+  ) => {
+    let menu: ReactElement;
+    switch (kind) {
       case NodeKind.Studio:
-        return 'home';
+        menu = genStudioMenu(data as StudioState);
+        break;
       case NodeKind.Song:
-        return 'music';
+        menu = genSongMenu(data as SongState);
+        break;
       case NodeKind.Version:
-        return 'document';
+        menu = genVerMenu(data as VersionState);
+        break;
       default:
-        return 'error';
+        menu = <span />;
+    }
+    return {
+      id: data.id,
+      icon: iconForNodeKind(kind),
+      label: data.name,
+      kind: kind,
+      // Doc says "selector function should be pure".
+      // Maybe better not to rely on local state?
+      // https://react-redux.js.org/api/hooks#useselector
+      isExpanded: !collapsedNodes.includes(data.id),
+      secondaryLabel: (
+        // Prevent click events from bubbling up to node component,
+        // which fire `onNodeClick` event handler.
+        <span onClick={(e) => e.stopPropagation()}>
+          <Popover minimal position={Position.RIGHT}>
+            <Icon
+              icon="more"
+              style={{ transform: 'rotate(90deg)', cursor: 'pointer' }}
+            />
+            {menu}
+          </Popover>
+        </span>
+      ),
+      data: data,
+      childNodes,
+    };
+  };
+
+  // Construct tree-like data structure for browser.
+  // Studio -> Song -> Version
+  const treeData: Node[] = useSelector((state: RootState) => {
+    return state.studios.allIds.map((id) => {
+      const studio = state.studios.byId[id];
+      const songs: Node[] = state.songs.allIds
+        .map((id) => {
+          const song = state.songs.byId[id];
+          const versions: Node[] = state.versions.allIds
+            .map((id) => {
+              const ver = state.versions.byId[id];
+              return genNode(ver, NodeKind.Version);
+            })
+            .filter((ver) => ver.data.songId === song.id);
+          return genNode(song, NodeKind.Song, versions);
+        })
+        .filter((song) => song.data.studioId === studio.id);
+      return genNode(studio, NodeKind.Studio, songs);
+    });
+  }, nodeListDeepEquals);
+
+  const handleNodeExpand = (node: ITreeNode) => {
+    node.isExpanded = true;
+    setCollapsedNodes(collapsedNodes.filter((id) => id !== node.id));
+  };
+
+  const handleNodeCollapse = (node: ITreeNode) => {
+    node.isExpanded = false;
+    setCollapsedNodes(collapsedNodes.concat([node.id.toString()]));
+  };
+
+  const handleNodeClick = (iTreeNode: ITreeNode) => {
+    const node = iTreeNode as Node;
+    switch (node.kind) {
+      case NodeKind.Version:
+        setVerId(node.id);
+        break;
+      default:
+        break;
     }
   };
 
-  const renderNode = (node: Node) => {
-    const renderFileFolderToolbar = (caption: string) => (
-      <Toolbar onClick={(e) => handleNodeClick(e, node)}>
-        <FloatLeft>
-          <Icon icon={iconForNodeKind(node.kind)} />
-          {caption}
-        </FloatLeft>
-        <ToolbarFileFolder id="folder-node">
-          {node.kind === NodeKind.Studio && (
-            <Popover minimal position={Position.RIGHT}>
-              <Icon icon="more" style={{ transform: 'rotate(90deg)' }} />
-              {genStudioMenu(node.data as StudioState)}
-            </Popover>
-          )}
-          {node.kind === NodeKind.Song && (
-            <Popover minimal position={Position.RIGHT}>
-              <Icon icon="more" style={{ transform: 'rotate(90deg)' }} />
-              {genSongMenu(node.data as SongState)}
-            </Popover>
-          )}
-          {node.kind === NodeKind.Version && (
-            <Popover minimal position={Position.RIGHT}>
-              <Icon icon="more" style={{ transform: 'rotate(90deg)' }} />
-              {genVerMenu(node.data as VersionState)}
-            </Popover>
-          )}
-        </ToolbarFileFolder>
-      </Toolbar>
-    );
-    return <div>{renderFileFolderToolbar(node.module)}</div>;
-  };
-
-  const tree = new Tree(treeData);
-  tree.renderNode = renderNode;
-  tree.updateNodesPosition();
   return (
     <div>
       {inviteDialog}
       {/* eslint-disable-next-line @typescript-eslint/no-use-before-define */}
       <Scrollbar style={Scroll} contentProps={styledScrollRenderer(Container)}>
-        <div className="m-tree">
-          <NodeUI
-            paddingLeft={20}
-            tree={tree}
-            index={tree.getIndex(1)}
-            key={1}
-            onChange={(tree: Node) => {
-              console.log(tree);
-            }}
-            renderNode={renderNode}
-          />
-        </div>
+        <Tree
+          contents={treeData}
+          onNodeExpand={handleNodeExpand}
+          onNodeCollapse={handleNodeCollapse}
+          onNodeClick={handleNodeClick}
+        />
       </Scrollbar>
     </div>
   );
@@ -338,36 +371,5 @@ const Container = {
 const Scroll = {
   width: 300,
 };
-
-const Toolbar = styled.div`
-  position: relative;
-  display: flex;
-  color: #d8e0f0;
-  z-index: +1;
-  /*border: 1px solid white;*/
-  padding-bottom: 4px;
-  i {
-    margin-right: 5px;
-    cursor: pointer;
-  }
-  i :hover {
-    color: #d8e0f0;
-  }
-`;
-
-const FloatLeft = styled.span`
-  padding-left: 4px;
-  width: 100%;
-`;
-
-const ToolbarFileFolder = styled.div`
-  position: absolute;
-  text-align: right;
-  width: 92%;
-  color: transparent;
-  &:hover {
-    color: #d8e0f0;
-  }
-`;
 
 export default Browser;
