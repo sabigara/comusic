820003d88cf52159dc95900e2aa9f67f0b0e7247
COEN330 <COEN330@users.noreply.github.com>
2020-04-29 17:44:05 +0900

    Fix the pointed out

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import { secondsToPixels } from '../common/conversions';
 import useAudioAPI from '../hooks/useAudioAPI';
+import { ReactComponent as Knob } from '../knob.svg';
 
 type Props = {
   offset?: number;
@@ -20,24 +21,12 @@ const Cursor: React.FC<Props> = ({ offset = 0 }) => {
   const left = secondsToPixels(time, 1000, audioAPI.sampleRate) + offset;
 
   return (
-    <Wrapper>
-      <Knob style={{ left: (left - 7).toString() + 'px' }}
-        src="knob.svg"></Knob>
+    <>
+      <Knob style={{ left: (left - 7).toString() + 'px', position: 'absolute', top: 0, width: 15, filter: 'drop-shadow(0px 0px 2px #000)' }}></Knob>
       <Line style={{ left: left.toString() + 'px' }}></Line>
-    </Wrapper>
+    </>
   )
 };
-
-const Wrapper = styled.div``;
-
-const Knob = styled.img`
-  position: absolute;
-  top: 0;
-  width: 15px;
-  top: 0;
-  filter: drop-shadow(0px 0px 2px #000);
-  user-select: none;
-  `;
 
 const Line = styled.div`
   position: absolute;
